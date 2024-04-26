87c28fb0f0dfb25b5d35c02fef3ff13ce0d3a414
Jeremmy <hizkiajeremmy@gmail.com>
2024-04-27 04:22:20 +0700

    hopefully missing images fixed

--- src/components/shared/BestProductCard.jsx
+++ src/components/shared/BestProductCard.jsx
@@ -5,6 +5,9 @@ const BestProductCard = ({ image, imageName, name }) => {
     <div className="flex flex-col w-[400px] h-72 justify-center items-center bg-transparent">
       <div
         className={`flex w-full h-5/6 rounded-2xl drop-shadow-xl bg-Product${imageName} bg-cover bg-center`}
+        style={{
+          backgroundImage: `url(${image})`,
+        }}
       />
       <div className="flex w-full h-1/6 font-Montserrat font-bold text-cust-dark-green text-center justify-center items-center">
         <div className="text-[14px] text-ellipsis overflow-hidden hyphens-auto max-w-full">{name}</div>
