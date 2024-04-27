c9ad763c6681b4bb7436a90d0c9bf0e0decd09a9
Jeremmy <hizkiajeremmy@gmail.com>
2024-04-27 18:28:06 +0700

    using img div instead of backgroundimage in products page

--- src/components/shared/BestProductCard.jsx
+++ src/components/shared/BestProductCard.jsx
@@ -3,11 +3,18 @@ import React from "react";
 const BestProductCard = ({ image, imageName, name }) => {
   return (
     <div className="flex flex-col w-[400px] h-72 justify-center items-center bg-transparent">
-      <div
-        className={`flex w-full h-5/6 rounded-2xl drop-shadow-xl bg-Product${imageName} bg-cover bg-center`}
-      />
+      {/* <div className={`flex w-full h-5/6 rounded-2xl drop-shadow-xl bg-Product${imageName} bg-cover bg-center`}/> */}
+      <div className="flex w-full h-5/6 rounded-2xl drop-shadow-xl overflow-hidden">
+        <img
+          className=" max-h-full min-w-full object-cover"
+          src={image}
+          alt="product image"
+        />
+      </div>
       <div className="flex w-full h-1/6 font-Montserrat font-bold text-cust-dark-green text-center justify-center items-center">
-        <div className="text-[14px] text-ellipsis overflow-hidden hyphens-auto max-w-full">{name}</div>
+        <div className="text-[14px] text-ellipsis overflow-hidden hyphens-auto max-w-full">
+          {name}
+        </div>
       </div>
     </div>
   );
--- src/components/shared/ProductCard.jsx
+++ src/components/shared/ProductCard.jsx
@@ -2,7 +2,6 @@
 import React from "react";
 
 const ProductCard = ({ image, name, description, isScaled, className }) => {
-
   return (
     <div
       className={`flex flex-col w-[352px] h-[500px] justify-center items-center bg-cust-dark-green text-white font-Montserrat px-10 py-12 rounded-[30px] gap-8 ${
@@ -12,11 +11,12 @@ const ProductCard = ({ image, name, description, isScaled, className }) => {
       {/* <div className={`flex w-60 h-64 bg-Product${name} ${className} bg-no-repeat bg-cover bg-center rounded-[24px]`}/> */}
       <div className="flex w-60 h-64 justify-center items-center overflow-hidden rounded-[24px]">
         <img
-          className=" min-h-64 min-w-60"
+          className="w-full h-full object-cover object-center"
           src={image}
           alt="product image"
         />
       </div>
+
       <div className="flex flex-col justify-center items-center gap-2">
         <div className="text-3xl font-bold">{name}</div>
         <div className="text-[16px] text-center">{description}</div>
